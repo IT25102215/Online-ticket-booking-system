fc217de4f8c2cc6b9593f4a6cbac5d971ea72b2a
IT25103642 <it25103642@my.sliit.lk>
2026-05-16 22:27:57 +0530

    user management

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.usermanagement.service;
+
+
+
+import com.example.usermanagement.model.User;
+import com.example.usermanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
